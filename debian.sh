17f965d204b080622e3b7d731d422d62c381b1e4
wahasa <69626847+wahasa@users.noreply.github.com>
2025-03-21 10:28:30 +0700

    debian.sh

--- debian.sh
+++ debian.sh
@@ -44,23 +44,24 @@ printf "${cyn}Select your debian < ${ylw}code name${cyn} > :${ylw}"
 read -p " " debian
 
 clear
-printf "${rst}\n"
-printf "${grn}Installing Debian,..\n"
-printf "${rst}\n"
-
-pkg install root-repo x11-repo
-pkg install proot neofetch pulseaudio -y
-#termux-setup-storage
 #debian=trixie
 #build=2025
+bin=.debian
+linux=debian
+     printf "${rst}\n"
+     printf "${grn}Installing $linux $ubuntu,..\n"
+     printf "${rst}\n"
+     pkg install root-repo x11-repo
+     pkg install proot neofetch pulseaudio -y
+     #termux-setup-storage
 echo ""
-neofetch --ascii_distro Debian -L
 folder=debian-fs
+neofetch --ascii_distro $linux -L
 if [ -d "$folder" ]; then
          first=1
          printf "${red}Skipping Downloading.${rst}\n"
 fi
-tarball="debian-rootfs.tar.xz"
+tarball="debian-rootfs.tar"
 if [ "$first" != 1 ];then
          if [ ! -f $tarball ]; then
          printf "${grn}Downloading rootfs, please wait,..${rst}\n"
@@ -87,11 +88,11 @@ if [ "$first" != 1 ];then
     echo "localhost" > $folder/etc/hostname
     echo "127.0.0.1 localhost" > $folder/etc/hosts
     echo "nameserver 8.8.8.8" > $folder/etc/resolv.conf
-bin=.debian
-linux=debian
-printf "\n"
-printf "${ppl}Writing launch script.${rst}\n"
-mkdir -p $folder/home/$linux
+    mkdir -p $folder/home/$linux
+    printf "\n"
+    printf "${ppl}Writing launch script.${rst}\n"
+    printf "\n"
+
 cat > $bin <<- EOM
 #!/data/data/com.termux/files/usr/bin/bash
 cd \$(dirname \$0)
@@ -100,8 +101,6 @@ pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth
 ## Unset LD_PRELOAD in case termux-exec is installed
 unset LD_PRELOAD
 command="proot"
-## Uncomment following line if you are having FATAL: kernel too old message.
-#command+=" -k 4.14.81"
 command+=" --link2symlink"
 command+=" --kill-on-exit"
 command+=" -0"
@@ -113,20 +112,24 @@ if [ -n "\$(ls -A $folder/binds)" ]; then
 fi
 command+=" -b /dev"
 command+=" -b /dev/urandom:/dev/random"
+command+=" -b /dev/null:/proc/sys/kernel/cap_last_cap"
 command+=" -b /proc"
 command+=" -b /proc/self/fd:/dev/fd"
 command+=" -b /proc/self/fd/0:/dev/stdin"
 command+=" -b /proc/self/fd/1:/dev/stdout"
 command+=" -b /proc/self/fd/2:/dev/stderr"
-command+=" -b $folder/home/$linux:/dev/shm"
+command+=" -b /sys"
+command+=" -b /data/data/com.termux"
+command+=" -b $folder/tmp:/dev/shm"
 ## Uncomment the following line to have access to the home directory of termux
-#command+=" -b /data/data/com.termux/files/home:/root"
+#command+=" -b /data/data/com.termux/files/home:/home/$linux"
 ## Uncomment the following line to mount /sdcard directly to /
 command+=" -b /sdcard"
+command+=" -b /mnt"
 command+=" -w /home/$linux"
 command+=" /usr/bin/env -i"
 command+=" HOME=/home/$linux"
-command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
+command+=" PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 command+=" TERM=\$TERM"
 command+=" LC_ALL=C"
 command+=" LANG=en_US.UTF-8"
@@ -139,22 +142,22 @@ else
    \$command -c "\$com"
 fi
 EOM
-     printf "\n"
      printf "${ylw}Fixing shebang of $linux.\n"
      termux-fix-shebang $bin
      printf "${ylw}Making executable $linux.\n"
      chmod +x $bin
      printf "${ylw}Fixing permissions $linux.\n"
      #chmod -R 755 $folder
-     printf "${ylw}Remove file rootfs $linux.\n"
+     printf "${ylw}Removing rootfs of $linux.\n"
      rm $tarball
      #clear
      printf "\n"
      printf "${red}Updating Package,..${rst}\n"
      printf "\n"
+
 cp $folder/etc/skel/.bashrc $folder/home/$linux/
-echo "TZ='Asia/Jakarta'; export TZ" >> $folder/home/$linux/.profile
-echo "export PULSE_SERVER=127.0.0.1" >> $folder/home/$linux/.bashrc
+#echo "TZ='Asia/Jakarta'; export TZ" >> $folder/home/$linux/.profile
+#echo "export PULSE_SERVER=127.0.0.1 ; export LANG=en_US.UTF-8" >> $folder/home/$linux/.bashrc
 #sed -i 's/32/31/g' $folder/home/$linux/.bashrc
 echo "" > $folder/home/$linux/.hushlogin
 
