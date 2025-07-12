ff6a5e09297252159a2161ddfbac1bf9b7c1420a
wahasa <69626847+wahasa@users.noreply.github.com>
2025-07-12 13:25:42 +0700

    ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -15,12 +15,12 @@
 # [+] noble  24.04 lts ( Noble Numbat )    - ( Released Apr 2024 / End of Legacy Support Apr 2036 )
 
 # Ubuntu Lastest
-# [+] oracular  24.10 ( Oracular Oriole )  - ( Released Oct 2024 / End of Standard Support Jul 2025 )
-# [+] plucky    25.04 ( Plucky Puffin )    - ( Released Apr 2025 / End of Standard Support jan 2026 )
+# [+] oracular   24.10 ( Oracular Oriole ) - ( Released Oct 2024 / End of Standard Support Jul 2025 )
+# [+] plucky     25.04 ( Plucky Puffin )   - ( Released Apr 2025 / End of Standard Support jan 2026 )
 
 # Ubuntu Next
-# [+] questing  25.10 ( Questing Quokka )  - ( Next Release )
-# [+] devel  ( Development )
+# [+] questing   25.10 ( Questing Quokka ) - ( Next Release )
+# [+] devel            ( Development )
 
 # Ubuntu Repositories
 # <=> http://ports.ubuntu.com/ubuntu-ports/dists
